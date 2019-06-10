8240175a950db011ccdd4582324be3a66901cc26
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-10 21:57:43 +1000

    pokemon now showing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AllPokemon from "./components/AllPokemon";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <AllPokemon />
+    </div>
+  );
 }
 
 export default App;
--- src/components/AllPokemon.js
+++ src/components/AllPokemon.js
@@ -1,14 +1,16 @@
 import React from "react";
 import axios from "axios";
+import PokemonCard from "./PokemonCard";
 
-export default class AllPokemon extends Component {
+export default class AllPokemon extends React.Component {
   state = {
-    url: "https://pokeapi.co/api/v2/pokemon/",
+    url: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151",
     pokemon: null
   };
 
   async componentDidMount() {
     const res = await axios.get(this.state.url);
+    console.log(res);
     this.setState({ pokemon: res.data["results"] });
   }
 
@@ -26,7 +28,7 @@ export default class AllPokemon extends Component {
             ))}
           </div>
         ) : (
-          <Loading />
+          <h1>loading</h1>
         )}
       </div>
     );
