90762829a914cc507ad250ee3612bfca1fb0b844
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-11 13:47:31 +1000

    broken? not sure whats wrong

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -6,14 +6,16 @@ export default class Pokedex extends React.Component {
     super(props);
 
     this.state = {
-      pokemon: []
+      pokemon: [],
+      error: null
     };
   }
   componentDidMount() {
     getPokemon()
       .then(data => {
         this.setState({
-          pokemon: data
+          pokemon: data,
+          error: null
         });
       })
       .catch(() => {
@@ -28,8 +30,14 @@ export default class Pokedex extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>hello</h1>
-        {console.log(this.state.pokemon)}
+        <h1>Pokedex</h1>
+        <ul>
+          {console.log(this.state.pokemon)}
+
+          {this.state.pokemon.map(poke => (
+            <li key={poke.name}>{poke.name}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
